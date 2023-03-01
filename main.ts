c255bc838b9811e294c8b66da4cdd6e6a9b4fd03
s21110 <122764722+s21110@users.noreply.github.com>
2023-03-01 14:11:38 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(walk)
 })
+input.onButtonPressed(Button.B, function () {
+    walk = 0
+})
 let walk = 0
 walk = 0
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 0) {
+    if (input.acceleration(Dimension.Strength) > 800) {
         walk += 1
-        basic.pause(100)
+        basic.pause(900)
     }
 })
