7fc8571df6562b6e2698c8c916c5540eb59b69b2
s21110 <122764722+s21110@users.noreply.github.com>
2023-03-02 10:53:39 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(walk)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function () {
+    run = 0
     walk = 0
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(run)
+})
+let run = 0
 let walk = 0
 walk = 0
+run = 0
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 800) {
+    if (input.acceleration(Dimension.Strength) > 700 && input.acceleration(Dimension.Strength) < 1000) {
         walk += 1
         basic.pause(900)
+    } else {
+        if (input.acceleration(Dimension.Strength) >= 1000 && input.acceleration(Dimension.Strength) < 1300) {
+            run += 1
+            basic.pause(900)
+        }
     }
 })
